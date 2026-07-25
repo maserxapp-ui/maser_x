ef823daaf5781f48b7c232ea2d513366235944f6
maserxapp-ui <maser.x.app@gmail.com>
2026-07-25 00:08:21 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -500,7 +500,7 @@ export default function App() {
     }
   };
 if (viewMode === 'user') {
-    return <UserViews supabase={supabase} onBackToAdmin={handleAdminAccess} />;
+    return <UserViews supabase={supabase} onBackToAdmin={handleAdminAccess} logoImg={logoImg} />;
   }
   return (
     <div className="flex h-screen bg-slate-100 font-['Tajawal',sans-serif] text-slate-800 dir-rtl" dir="rtl">
