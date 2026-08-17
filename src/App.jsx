88527f086fe2a947955c1bf39366e8e94e17e7bf
maserxapp-ui <maser.x.app@gmail.com>
2026-08-17 00:14:29 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -148,6 +148,10 @@ export default function App() {
           is_dropped_return: false,
           tomorrow_status: null,
           exam_note: null
+          driver_name: null,         // تصفية اسم السائق
+          driver_phone: null,        // تصفية رقم السائق
+          assigned_driver: null,     // تصفية السائق المخصص
+          assignment_status: null,
         })
         .not('id', 'is', null);
 
