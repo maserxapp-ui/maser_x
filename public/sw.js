87fdbd7f12b4f2561ee90efe5754b74359466ad6
maserxapp-ui <maser.x.app@gmail.com>
2026-09-22 09:10:26 -0700

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,20 @@
+// ملف Service Worker لتفعيل الإشعارات على أندرويد
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({ type: 'window' }).then((clientList) => {
+      if (clientList.length > 0) {
+        return clientList[0].focus();
+      }
+      return clients.openWindow('/');
+    })
+  );
+});
