a287940773253ce511881ab441ac86bdbaa37486
maserxapp-ui <maser.x.app@gmail.com>
2026-09-23 04:59:27 -0700

    Update send-notification.js

--- functions/api/send-notification.js
+++ functions/api/send-notification.js
@@ -3,11 +3,10 @@ export async function onRequestPost(context) {
     const body = await context.request.json().catch(() => ({}));
     const messageText = body.messageText || "🚗 السائق في طريقه إليكم الآن";
 
-    // المفتاح الجديد الخاص بكِ من الشاشة الأخيرة
     const apiKey = "os_v2_app_yboihim2jzb6zfcksv6qkhtrsiyt4t5e2oqubt5mpcgoagxtpgnh6dm2cq464243nk65oxmd77x1mlknm4zaexntijfvwykn3mlft5a";
     const appId = "c05c83a1-9a4e-43ec-944a-957d051e7192";
 
-    const response = await fetch("https://onesignal.com/api/v1/notifications", {
+    const response = await fetch("https://api.onesignal.com/notifications", {
       method: "POST",
       headers: {
         "Content-Type": "application/json; charset=utf-8",
